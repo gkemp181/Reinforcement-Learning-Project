29410942f9a26598e80b32a6d423b901ea2b98eb
Garrett Kemper <gkemp181@gmail.com>
2025-04-18 13:14:19 -0400

    changes to testing.py

--- testing.py
+++ testing.py
@@ -1,27 +1,3 @@
-# import gymnasium as gym
-# import numpy as np
-
-# import gymnasium_robotics
-
-# gym.register_envs(gymnasium_robotics)
-
-# env = gym.make(
-#     'FetchPickAndPlace-v3',
-#     xml_file='./xarm7ELE392.xml',
-#     forward_reward_weight=1,
-#     ctrl_cost_weight=0.05,
-#     contact_cost_weight=5e-4,
-#     healthy_reward=1,
-#     main_body=1,
-#     healthy_z_range=(0.195, 0.75),
-#     include_cfrc_ext_in_observation=True,
-#     exclude_current_positions_from_observation=False,
-#     reset_noise_scale=0.1,
-#     frame_skip=25,
-#     max_episode_steps=1000,
-# )
-
-
 import gymnasium as gym
 import gymnasium_robotics
 from stable_baselines3 import PPO
