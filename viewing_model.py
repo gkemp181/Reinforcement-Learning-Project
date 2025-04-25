09868846659ca511be836d5da2a4f51059b1b6da
Jarrett DeFreitas <jarrett.defreitas@uri.edu>
2025-04-25 17:37:57 -0400

    Pointless changes

--- viewing_model.py
+++ viewing_model.py
@@ -2,13 +2,16 @@ import gymnasium as gym
 import time
 import numpy as np
 from stable_baselines3 import SAC  # or another algorithm like SAC or TD3
+from create_env import create_env
+
+env = create_env(render_mode="human")
 
 # Load the MuJoCo "Pick and Place" environment
-env = gym.make("FetchPickAndPlace-v2", render_mode="human")  # "human" for real-time rendering
+gym.make(env, env_id="FetchPickAndPlace-v3", render_mode="human")  
 
 # Load the pretrained model
 # Replace 'path_to_model.zip' with the actual path to your trained agent
-model = SAC.load("C:\Users\defre\OneDrive\Desktop\ELE392Project\Reinforcement-Learning-Project\models\recording_test1.zip")
+model = SAC.load("Reinforcement-Learning-Project\models\recording_test1.zip")
 
 obs, _ = env.reset()
 
